3cf9b1eaaa37ad80c6df004020503cef384eb9cd
Dmitry Kozlov <kozlov.dmitry.a@gmail.com>
2025-02-11 23:24:17 +0300

    More useful primitives for datalog db
    Sketch an idea of how storage will be organized.
    Equality has importance for joining and querying relations,
    hence it's beneficial to get rid of the structure of constants and
    store them as Id values in the value domain.
    Bdddb uses this for storing relations as binary decision diagrams.
    TODO decide which data structure to use, temporary using ArrayList to
    get a working semi-naive evaluator first.

--- src/database.zig
+++ src/database.zig
@@ -1,90 +1,107 @@
 const std = @import("std");
+
 const Allocator = std.mem.Allocator;
 const ArenaAllocator = std.heap.ArenaAllocator;
-const HashMapUnmanaged = std.HashMapUnmanaged;
 
-const Literal = []const u8;
-const Relation = struct { arity: u32, name: []const u8 };
-const RelationHashEql = struct {
-    pub fn hash(_: @This(), rel: Relation) u64 {
-        return std.hash.Wyhash.hash(rel.arity, rel.name);
-    }
-    pub fn eql(_: @This(), this: Relation, that: Relation) bool {
-        return this.arity == that.arity and std.mem.eql(u8, this.name, that.name);
-    }
-};
-const Db = struct {
-    arena: ArenaAllocator,
-    relations: HashMapUnmanaged(
-        Relation,
-        RelationImplementation,
-        RelationHashEql,
-        std.hash_map.default_max_load_percentage,
-    ),
-};
-
-const QTuple = []const ?Literal;
-const RTuple = []const Literal;
-
-const RelationImplementation = union(enum) {
-    memory_seq: struct {
-        relation: std.ArrayList(RTuple),
-    },
-    fn query(self: *RelationImplementation, tuple: QTuple) RelationIterator {
-        return switch (self.*) {
-            .memory_seq => |*rel| RelationIterator{
-                .memory_seq = .{
-                    .relation = &rel.relation,
-                    .query = tuple,
-                    .cursor = 0,
-                },
+const DomainValue = union(enum) {
+    seq: []const u8,
+    fn clone(self: DomainValue, allocator: Allocator) Allocator.Error!DomainValue {
+        switch (self) {
+            .seq => |seq| {
+                const target = try allocator.alloc(u8, seq.len);
+                @memcpy(target, seq);
+                return DomainValue{ .seq = target };
             },
-        };
-    }
-};
-
-const RelationIterator = union(enum) {
-    memory_seq: MemorySeqIterator,
-    fn next(self: *RelationIterator) ?RTuple {
-        return switch (self.*) {
-            .memory_seq => |*impl| impl.next(),
-        };
-    }
-};
-
-const MemorySeqIterator = struct {
-    relation: *std.ArrayList(RTuple),
-    query: QTuple,
-    cursor: u32,
-    fn next(self: *MemorySeqIterator) ?RTuple {
-        loop: while (self.cursor < self.relation.items.len) {
-            const tuple = self.relation.items[self.cursor];
-            self.cursor += 1;
-            for (self.query, tuple) |q, t| {
-                if (q) |spec| {
-                    if (!std.mem.eql(u8, spec, t)) continue :loop;
-                }
-            }
-            return tuple;
         }
-        return null;
+    }
+    fn destroy(self: DomainValue, allocator: Allocator) void {
+        switch (self) {
+            .seq => |seq| allocator.free(seq),
+        }
+    }
+};
+const DomainValueHashEql = struct {
+    pub fn eql(_: @This(), this: DomainValue, that: DomainValue) bool {
+        return std.meta.eql(this, that);
+    }
+    pub fn hash(_: @This(), this: DomainValue) u64 {
+        switch (this) {
+            .seq => |seq| return std.hash.Wyhash.hash(0, seq),
+        }
     }
 };
 
-test "Db" {
+/// Stealing this cool technique from bdddb:
+/// instead of using multiple copies of the data and wasting CPU cycles on equality checks
+/// store every encountered value in a unified domain index and assign a word id to it.
+/// Highly unlikely that a dataset for any program exceeds word size (?).
+const Domain = struct {
+    /// Next unique index available for a domain value.
+    sequence: u64 = 0,
+    index: std.HashMap(DomainValue, u64, DomainValueHashEql, std.hash_map.default_max_load_percentage),
+    /// Own, hash, and assign an unique id to this domain value.
+    fn register(self: *Domain, value: DomainValue) Allocator.Error!u64 {
+        const id = self.sequence;
+        const copy = try value.clone(self.index.allocator);
+        errdefer copy.destroy(self.index.allocator);
+        try self.index.put(value, id);
+        self.sequence += 1;
+        return id;
+    }
+};
+
+const ArrayListStorage = struct {
+    arity: u32,
+    backend: std.ArrayList(u64),
+    fn insert(self: *ArrayListStorage, tuple: []const u64) Allocator.Error!void {
+        std.debug.assert(self.arity == tuple.len);
+        return self.backend.appendSlice(tuple);
+    }
+    fn iterator(self: *ArrayListStorage, query: []const ?u64) Iterator {
+        return Iterator{
+            .index = 0,
+            .arity = self.arity,
+            .query = query,
+            .backend = &self.backend,
+        };
+    }
+    const Iterator = struct {
+        index: u32,
+        arity: u32,
+        query: []const ?u64,
+        backend: *std.ArrayList(u64),
+        fn next(self: *Iterator) ?[]const u64 {
+            loop: while (self.index * self.arity < self.backend.items.len) : (self.index += 1) {
+                const i = self.index;
+                const a = self.arity;
+                const slice = self.backend.items[i * a .. i * a + a];
+                for (0..self.arity) |j| {
+                    if (self.query[j]) |x| {
+                        if (x != slice[j]) continue :loop;
+                    }
+                }
+                self.index += 1;
+                return slice;
+            }
+            return null;
+        }
+    };
+};
+
+test "ArrayListStorage" {
     var arena = ArenaAllocator.init(std.testing.allocator);
     defer arena.deinit();
-    var relation = std.ArrayList(RTuple).init(arena.allocator());
-    try relation.append(&[_]Literal{ "a", "b" });
-    try relation.append(&[_]Literal{ "a", "c" });
-    try relation.append(&[_]Literal{ "c", "z" });
-    var impl = RelationImplementation{ .memory_seq = .{ .relation = relation } };
-    var it = impl.query(&[_]?Literal{ "a", null });
-    while (it.next()) |tuple| {
-        try std.json.stringify(
-            tuple,
-            .{},
-            std.io.getStdErr().writer(),
-        );
+    var storage = ArrayListStorage{
+        .arity = 2,
+        .backend = std.ArrayList(u64).init(arena.allocator()),
+    };
+    try storage.insert(&[_]u64{ 1, 0 });
+    try storage.insert(&[_]u64{ 1, 1 });
+    try storage.insert(&[_]u64{ 1, 2 });
+    try storage.insert(&[_]u64{ 2, 0 });
+    try storage.insert(&[_]u64{ 2, 1 });
+    var it = storage.iterator(&[_]?u64{ 2, null });
+    while (it.next()) |t| {
+        std.debug.print("{any}\n", .{t});
     }
 }
